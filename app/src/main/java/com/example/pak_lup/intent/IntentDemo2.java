b663067b0e76fa4eb076656b9b83857db100ad11
gdmec07150913 <408594990@qq.com>
2016-10-30 18:45:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/pak_lup/intent/IntentDemo2.java
@@ -0,0 +1,30 @@
+package com.example.pak_lup.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Pak_Lup on 2016/10/27 0027.
+ */
+
+public class IntentDemo2 extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_demo2);
+    }
+
+    public void upper(View v){
+        Intent intent=getIntent();
+        Bundle bundle=intent.getExtras();
+        final String value=bundle.getString("value").toUpperCase();
+
+        Intent intentresult=new Intent();
+        intentresult.putExtra("result",value);
+        setResult(RESULT_OK,intentresult);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/pak_lup/intent/IntentDemo3.java
@@ -0,0 +1,17 @@
+package com.example.pak_lup.intent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Pak_Lup on 2016/10/27 0027.
+ */
+
+public class IntentDemo3 extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_demo3);
+    }
+}
